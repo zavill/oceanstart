64ce62a3cd37f07e9beb1ad5013ff57e6c712363
Daniil Zavilohin <denik6740@gmail.com>
2021-07-13 17:56:30 +0300

    Создан абстрактный класс для API

---
+++ src/Controller/API/AbstractAPI.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace App\Controller\API;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+/**
+ * Абстрактный класс для API
+ *
+ * Class AbstractApi
+ * @package App\Controller\API
+ */
+abstract class AbstractAPI extends AbstractController
+{
+
+    protected ?Request $request;
+
+    protected ObjectManager $entityManager;
+
+    /**
+     * AbstractApi constructor.
+     * @param RequestStack $requestStack
+     * @param EntityManagerInterface $entityManager
+     */
+    public function __construct(
+        RequestStack $requestStack,
+        EntityManagerInterface $entityManager
+    ) {
+        $this->request = $requestStack->getCurrentRequest();
+
+        $this->entityManager = $entityManager;
+    }
+}

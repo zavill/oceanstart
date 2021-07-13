3e99fee4ea08ad8623b422166274b5b5e27c7687
Daniil Zavilohin <denik6740@gmail.com>
2021-07-14 01:45:35 +0300

    Рефакторинг JS детальной информации о книге

--- assets/src/js/partials/components/detail-book.js
+++ assets/src/js/partials/components/detail-book.js
@@ -93,33 +93,3 @@ function addBook() {
         }
     )
 }
-
-function prepareBookDOM(data) {
-    let coAuthorsText = '';
-    if (data.coAuthor) {
-        let coAuthorsLength = data['coAuthor'].length - 1;
-        data.coAuthor.forEach(function (elem, index) {
-            coAuthorsText += elem;
-            if (index === coAuthorsLength) {
-                coAuthorsText += '. ';
-            } else {
-                coAuthorsText += ', ';
-            }
-        });
-    }
-    let openTag, closeTag;
-    if (isAdmin) {
-        openTag = '<li><a href="/admin/book/'+data['id']+'">';
-        closeTag = '</a></li>';
-    } else {
-        openTag = '<li>';
-        closeTag = '</li>';
-    }
-    return openTag +
-        data['name'] + '. ' +
-        data['author'] + '. ' +
-        coAuthorsText +
-        data['shortDescription'] + '. ' +
-        data['publishDate'] + '.'
-    closeTag;
-}
